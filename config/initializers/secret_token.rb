cf27f5900bcd51118aede9103df20f9ad42e805e
Amy G Katriel <amygk@me.com>
2012-07-21 16:14:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RavelryApp::Application.config.secret_token = 'b768e638268546640017fe6a91b625fb7adfb6238e72a00ee2cbe68dee039472b3a07adbb53473b221deeed22b6a50d8fb31a4b3d1d7e4547bd010b1d3019b0f'
