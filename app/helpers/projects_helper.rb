288234524490c29820e38ade9672090a3d5ff5d9
Amy G Katriel <amygk@me.com>
2012-07-22 19:19:11 -0400

    first working version - yay

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,2 @@
 module ProjectsHelper
-
-require 'httparty'
-
-class RavelryT
-  include HTTParty
-  base_uri 'api.ravelry.com'
-  format :json
-
-  def self.get_rav_projects(user, key)
-    status = "frogged in-progress hibernating finished"
-    get('/projects/'+user+'/progress.json', :query => {:key => key, :status => status, :notes => 'false' })
-  end
-end
-
-
 end
